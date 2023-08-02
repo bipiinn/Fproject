c646ea6f1f27d8870e0692e059c11e28571b9404
bipiinn <bgurung3@my.centennialcollege.ca>
2023-08-01 22:59:41 -0400

    Group project

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb+srv://admin:admin@mongodbserver.lkz5jhf.mongodb.net/grocery_store?retryWrites=true&w=majority"
+    "URI": "mongodb+srv://bipindon:<!D0ntn1dp@ssw0rd>@cluster0.ju3yfnp.mongodb.net/"
     // "URI": "mongodb://127.0.0.1/grocery_store"
 }
